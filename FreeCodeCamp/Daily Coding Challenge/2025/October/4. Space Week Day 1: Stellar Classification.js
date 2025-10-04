c1d88eb1b70fdb4d19bc7fdbe6e925af3898183b
persstpancras <persstpancras@gmail.com>
2025-10-04 17:40:19 +0100

    Update 4. Space Week Day 1: Stellar Classification.js

--- FreeCodeCamp/Daily Coding Challenge/2025/October/4. Space Week Day 1: Stellar Classification.js
+++ FreeCodeCamp/Daily Coding Challenge/2025/October/4. Space Week Day 1: Stellar Classification.js
@@ -1,5 +1,17 @@
 function classification(temp) {
   if(temp < 3700) {
-    
-  
+    return "M"
+  } else if(temp < 5200) {
+    return "K"
+  } else if(temp < 6000) {
+    return "G"
+  } else if(temp < 7500) {
+  return "F"
+  } else if(temp < 10000) {
+    return "A"
+  } else if (temp < 30000) {
+    return "B"
+  } else {
+    return "O"
+  }
 }
